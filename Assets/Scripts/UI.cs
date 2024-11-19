8bcb6b4b4dea7011021b0e20d62a0094fea82027
gastenzijncool <97092382@st.deltion.nl>
2024-11-19 13:38:35 +0100

    Basic UI

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,20 @@
+using Meta.XR.InputActions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UI : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
