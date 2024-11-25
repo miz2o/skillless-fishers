d3618d9c4f789e3ba013f78c738f666e9532c1dd
miz2o <mizzosaur@Gmail.com>
2024-11-25 13:53:28 +0100

    scripts

---
+++ Assets/game/Scripts/FishInfo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishInfo: MonoBehaviour
+{
+    public int Index;
+    public string Name;
+    public string Description;
+    public float Strength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/game/Scripts/OVRRemakes/CustomGrabbable.cs
+++ Assets/game/Scripts/OVRRemakes/CustomGrabbable.cs
@@ -7,9 +7,16 @@ public class CustomGrabbable : OVRGrabbable
     // Variable to store the offset between the hand and handle when grabbed
     private Vector3 grabOffset;
 
+
+    public GameObject leftInfoMenu;
+    public GameObject rightInfoMenu;
+    public GameObject currentMenu;
+
+
     // Override GrabBegin method to handle the grab logic
     public override void GrabBegin(OVRGrabber hand, Collider grabPointy)
-    { 
+    {
+        print("GrabBegin");
 
         if (grabPointy != null)
         {
@@ -17,7 +24,35 @@ public class CustomGrabbable : OVRGrabbable
             gameObject.GetComponent<Rigidbody>().isKinematic = true;
             transform.position = hand.gameObject.transform.position;
             transform.rotation = hand.gameObject.transform.rotation;
-        } 
+        }
+        print("grabbing");
+        if (gameObject.tag == "Fish")
+        {
+            print("Holding fish");
+            if (leftInfoMenu && rightInfoMenu != null)
+            {
+                if(hand.gameObject.tag == "LeftHand")
+                {
+                    print("Left hand");
+                    leftInfoMenu.SetActive(true);
+                    currentMenu = leftInfoMenu;
+                }
+                else if (hand.gameObject.tag == "RightHand")
+                {
+                    print("Right hand");
+                    rightInfoMenu.SetActive(true);
+                    currentMenu = rightInfoMenu;
+                }
+                else
+                {
+                    print("Other tag");
+                    return;
+                }
+                
+            }
+        }
 
     }
+    
+    
 }
